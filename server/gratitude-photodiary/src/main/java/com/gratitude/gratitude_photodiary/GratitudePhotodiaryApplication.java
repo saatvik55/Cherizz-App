b0519c9edb4b912b142ea99e432d92d1829eb469
saatvik55 <saatviksethi55@gmail.com>
2024-12-22 02:11:13 +0530

    Server Initialisation

---
+++ server/gratitude-photodiary/src/main/java/com/gratitude/gratitude_photodiary/GratitudePhotodiaryApplication.java
@@ -0,0 +1,13 @@
+package com.gratitude.gratitude_photodiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GratitudePhotodiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GratitudePhotodiaryApplication.class, args);
+	}
+
+}
---
+++ server/gratitude-photodiary/src/test/java/com/gratitude/gratitude_photodiary/GratitudePhotodiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gratitude.gratitude_photodiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GratitudePhotodiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
